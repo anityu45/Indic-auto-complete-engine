e6581b3b7d0e3ce9ae2e8b889c23a0abc226a88e
Anirudh Ani <anianirudh1963538@gmail.com>
2025-08-27 14:49:59 +0530

    Updated Trie.cpp

--- Trie.cpp
+++ Trie.cpp
@@ -1,103 +1,62 @@
-// Trie implementation
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct  Node
-{
-    Node*links[26];
-    bool flag=false;
+struct Node {
+    Node* links[26];
+    bool flag = false;
 
-    bool containsKey(char ch){
-        return links[ch-'a']!=NULL;
+    bool containsKey(char ch) {
+        return links[ch - 'a'] != NULL;
     }
-    void add(char ch,Node* node){
-        links[ch-'a']=node;
+    void add(char ch, Node* node) {
+        links[ch - 'a'] = node;
     }
-    Node* get(char ch){
-        return links[ch-'a'];
+    Node* get(char ch) {
+        return links[ch - 'a'];
     }
-    bool setEnd(){
-        flag=true;
+    void setEnd() {
+        flag = true;
     }
-    bool isEnd(){
+    bool isEnd() {
         return flag;
     }
 };
 
-class Trie{
-    private: 
-        Node*root;
+class Trie {
+public:
+    using Node = ::Node;
+    Node* root;
 
-        void dfs(Node* node, string word, vector<string>& result){
-            if (node->isEnd()) result.push_back(word);
-            for (char ch = 'a'; ch <= 'z'; ch++) {
-                if (node->containsKey(ch)){
-                dfs(node->get(ch), word + ch, result);
-                }
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(const string& word) {
+        Node* node = root;
+        for (char ch : word) {
+            if (!node->containsKey(ch)) {
+                node->add(ch, new Node());
             }
+            node = node->get(ch);
         }
+        node->setEnd();
+    }
 
-    public:
-
-        Trie(){
-            root=new Node();
+    bool search(const string& word) {
+        Node* node = root;
+        for (char ch : word) {
+            if (!node->containsKey(ch)) return false;
+            node = node->get(ch);
         }
-        
-        void insert(string word){
-            Node* node=root;
-            for(int i=0;i<word.size();i++){
-                if(!node->containsKey(word[i])){
-                    node->add(word[i],new Node());
-                }
-                node=node->get(word[i]);
-            }
-            node->setEnd();
-        }
+        return node->isEnd();
+    }
 
-        bool search(string word){
-            Node*node=root;
-            for(int i=0;i<word.size();i++){
-                if(!node->containsKey(word[i])){
-                    return false;
-                }
-                node=node->get(word[i]);
-            }
-            return node->isEnd();
+    bool startswith(const string& prefix) {
+        Node* node = root;
+        for (char ch : prefix) {
+            if (!node->containsKey(ch)) return false;
+            node = node->get(ch);
         }
-
-        bool startswith(string prefix){
-            Node*node=root;
-            for(int i=0;i<prefix.size();i++){
-                if(!node->containsKey(prefix[i])){
-                    return false;
-                }
-                node=node->get(prefix[i]);
-            }
-            return true;
-        }
-
-        vector<string> wordsWithPrefix(string prefix) {
-            vector<string> result;
-            if (!startswith(prefix)) return result;
-            Node* node = root;
-            for (char c : prefix){
-                node = node->get(c);
-            }
-            dfs(node, prefix, result);
-            return result;
-        }
-
+        return true;
+    }
 };
-int main() {
-    Trie trie;
-    trie.insert("apple");
-    trie.insert("app");
-    trie.insert("apps");
-    trie.insert("battle");
-    trie.insert("bat");
-
-    vector<string> words = trie.wordsWithPrefix("ap");
-    for (string s : words) cout << s << endl;
-
-    return 0;
-}
