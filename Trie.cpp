25420c5eef5a925f0955d23b69c5d3f70a252132
Teja <pannem.1.chandrateja@gmail.com>
2025-08-27 19:00:02 +0530

    Isolated the trie to basic functions

--- Trie.cpp
+++ Trie.cpp
@@ -1,103 +1,91 @@
 // Trie implementation
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct  Node
-{
-    Node*links[26];
-    bool flag=false;
+class Trie {
+public:
+    struct Node {
+        Node* links[26] = {nullptr};
+        bool flag = false;
 
-    bool containsKey(char ch){
-        return links[ch-'a']!=NULL;
-    }
-    void add(char ch,Node* node){
-        links[ch-'a']=node;
-    }
-    Node* get(char ch){
-        return links[ch-'a'];
-    }
-    bool setEnd(){
-        flag=true;
-    }
-    bool isEnd(){
-        return flag;
-    }
-};
+        bool containsKey(char ch) {
+            return links[ch - 'a'] != nullptr;
+        }
+        void add(char ch, Node* node) {
+            links[ch - 'a'] = node;
+        }
+        Node* get(char ch) {
+            return links[ch - 'a'];
+        }
+        void setEnd() { // changed to void
+            flag = true;
+        }
+        bool isEnd() {
+            return flag;
+        }
+    };
 
-class Trie{
-    private: 
-        Node*root;
+    Node* root; // made public
 
-        void dfs(Node* node, string word, vector<string>& result){
-            if (node->isEnd()) result.push_back(word);
-            for (char ch = 'a'; ch <= 'z'; ch++) {
-                if (node->containsKey(ch)){
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(string word) {
+        Node* node = root;
+        for (int i = 0; i < word.size(); i++) {
+            if (!node->containsKey(word[i])) {
+                node->add(word[i], new Node());
+            }
+            node = node->get(word[i]);
+        }
+        node->setEnd();
+    }
+
+    bool search(string word) {
+        Node* node = root;
+        for (int i = 0; i < word.size(); i++) {
+            if (!node->containsKey(word[i])) {
+                return false;
+            }
+            node = node->get(word[i]);
+        }
+        return node->isEnd();
+    }
+
+    bool startswith(string prefix) {
+        Node* node = root;
+        for (int i = 0; i < prefix.size(); i++) {
+            if (!node->containsKey(prefix[i])) {
+                return false;
+            }
+            node = node->get(prefix[i]);
+        }
+        return true;
+    }
+
+    vector<string> wordsWithPrefix(string prefix) {
+        vector<string> result;
+        if (!startswith(prefix)) return result;
+        Node* node = root;
+        for (char c : prefix) {
+            node = node->get(c);
+        }
+        dfs(node, prefix, result);
+        return result;
+    }
+
+private:
+    void dfs(Node* node, string word, vector<string>& result) {
+        if (node->isEnd()) result.push_back(word);
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            if (node->containsKey(ch)) {
                 dfs(node->get(ch), word + ch, result);
-                }
             }
         }
-
-    public:
-
-        Trie(){
-            root=new Node();
-        }
-        
-        void insert(string word){
-            Node* node=root;
-            for(int i=0;i<word.size();i++){
-                if(!node->containsKey(word[i])){
-                    node->add(word[i],new Node());
-                }
-                node=node->get(word[i]);
-            }
-            node->setEnd();
-        }
-
-        bool search(string word){
-            Node*node=root;
-            for(int i=0;i<word.size();i++){
-                if(!node->containsKey(word[i])){
-                    return false;
-                }
-                node=node->get(word[i]);
-            }
-            return node->isEnd();
-        }
-
-        bool startswith(string prefix){
-            Node*node=root;
-            for(int i=0;i<prefix.size();i++){
-                if(!node->containsKey(prefix[i])){
-                    return false;
-                }
-                node=node->get(prefix[i]);
-            }
-            return true;
-        }
-
-        vector<string> wordsWithPrefix(string prefix) {
-            vector<string> result;
-            if (!startswith(prefix)) return result;
-            Node* node = root;
-            for (char c : prefix){
-                node = node->get(c);
-            }
-            dfs(node, prefix, result);
-            return result;
-        }
-
+    }
 };
-int main() {
-    Trie trie;
-    trie.insert("apple");
-    trie.insert("app");
-    trie.insert("apps");
-    trie.insert("battle");
-    trie.insert("bat");
 
-    vector<string> words = trie.wordsWithPrefix("ap");
-    for (string s : words) cout << s << endl;
+// Remove main() from Trie.cpp if you want to use it as a library
 
-    return 0;
-}
+
